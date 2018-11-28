0394bcf18b33616c6de73ffd5c7f2707c396c118
Ehsan <e.hosseini.ir@gmail.com>
2018-11-28 12:48:39 +0330

    Add test files and install packages

---
+++ wallaby.js
@@ -0,0 +1,15 @@
+module.exports = function () {
+  return {
+    files: [
+      'src/**/*.js'
+    ],
+
+    tests: [
+      'test/**/*Spec.js'
+    ],
+
+    env: {
+      type: 'node'
+    }
+  };
+};
